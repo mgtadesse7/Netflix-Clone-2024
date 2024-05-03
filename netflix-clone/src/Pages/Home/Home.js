dd402e27c64988235d32acbbcc2178528052c704
mgtadesse7 <mgtadesse7@gmail.com>
2024-05-02 21:08:31 -0500

    banner

--- netflix-clone/src/Pages/Home/Home.js
+++ netflix-clone/src/Pages/Home/Home.js
@@ -1,10 +1,12 @@
 import React from "react";
+import Banner from "../../components/Banner/Banner";
 import Footer from "../../components/Footer/Footer";
 import Header from "../../components/Header/Header";
 
 const Home = () => {
   return <div>
     <Header/>
+    <Banner/>
     <Footer/>
   </div>;
 };
